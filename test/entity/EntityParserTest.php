5bb5899d88244eecd264ef2d816a06091b42caa3
chkt <ck@chkt.one>
2017-08-14 11:50:43 +0200

    Add EntityParserTest

---
+++ test/entity/EntityParserTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace test\entity;
+
+use PHPUnit\Framework\TestCase;
+
+use eve\entity\EntityParser;
+
+
+
+final class EntityParserTest
+extends TestCase
+{
+
+	public function _produceInstance() {
+		return new EntityParser();
+	}
+
+
+	public function testParse() {
+		$ins = $this->_produceInstance();
+
+		$this->assertEquals([
+			'type' => 'foo',
+			'location' => 'bar'
+		], $ins->parse('foo:bar'));
+	}
+
+	public function testParse_empty() {
+		$ins = $this->_produceInstance();
+
+		$this->expectException(\ErrorException::class);
+		$this->expectExceptionMessage('ENT malformed entity ""');
+
+		$ins->parse('');
+	}
+
+	public function testParse_noType() {
+		$ins = $this->_produceInstance();
+
+		$this->expectException(\ErrorException::class);
+		$this->expectExceptionMessage('ENT malformed entity "bar"');
+
+		$ins->parse('bar');
+	}
+
+	public function testParse_emptyType() {
+		$ins = $this->_produceInstance();
+
+		$this->expectException(\ErrorException::class);
+		$this->expectExceptionMessage('ENT malformed entity ":bar"');
+
+		$ins->parse(':bar');
+	}
+
+	public function testParse_emptyLocation() {
+		$ins = $this->_produceInstance();
+
+		$this->assertEquals([
+			'type' => 'foo',
+			'location' => ''
+		], $ins->parse('foo:'));
+	}
+}
