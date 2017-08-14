8c2a735104ed2585db6f857ac91a2f2837b41d3b
chkt <ck@chkt.one>
2017-08-14 11:56:56 +0200

    Add ArgumentResolverTest

---
+++ test/inject/resolve/ArgumentResolverTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace test\inject\resolve;
+
+use PHPUnit\Framework\TestCase;
+
+use eve\common\IAccessorFactory;
+use eve\access\TraversableAccessor;
+use eve\driver\IInjectorDriver;
+use eve\inject\IInjectable;
+use eve\inject\resolve\IInjectorResolver;
+use eve\inject\resolve\ArgumentResolver;
+
+
+
+class ArgumentResolverTest
+extends TestCase
+{
+
+	private function _produceAccessor(array $data) {
+		return new TraversableAccessor($data);
+	}
+
+	private function _produceResolver() {
+		return new ArgumentResolver();
+	}
+
+	public function testDependencyConfig() {
+		$this->assertEquals([], ArgumentResolver::getDependencyConfig($this->_produceAccessor([])));
+	}
+
+	public function testInheritance() {
+		$resolver = $this->_produceResolver();
+
+		$this->assertInstanceOf(IInjectorResolver::class, $resolver);
+		$this->assertInstanceOf(IInjectable::class, $resolver);
+		$this->assertInstanceOf(IAccessorFactory::class, $resolver);
+	}
+
+	public function testProduce() {
+		$resolver = $this->_produceResolver();
+		$accessor = $this->_produceAccessor([ 'data' => 1 ]);
+
+		$this->assertEquals(1, $resolver->produce($accessor));
+	}
+
+	public function testProduce_invalidAccessor() {
+		$resolver = $this->_produceResolver();
+		$accessor = $this->_produceAccessor([]);
+
+		$this->expectException(\ErrorException::class);
+		$this->expectExceptionMessage('ACC invalid key "data"');
+
+		$resolver->produce($accessor);
+	}
+}
