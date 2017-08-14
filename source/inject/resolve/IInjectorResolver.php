d9b82a51a23ad4446f8eaa7126ee34748176fcab
chkt <ck@chkt.one>
2017-08-14 11:58:43 +0200

    Add IInjectorResolver

---
+++ source/inject/resolve/IInjectorResolver.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace eve\inject\resolve;
+
+use eve\common\IAccessorFactory;
+use eve\inject\IInjectable;
+
+
+
+interface IInjectorResolver
+extends IInjectable, IAccessorFactory
+{
+
+}
