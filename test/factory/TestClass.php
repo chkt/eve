60d4641cf74462ca817797314278fb1849e596c7
chkt <ck@chkt.one>
2017-08-14 11:40:22 +0200

    Add test objects

---
+++ test/factory/TestClass.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace test\factory;
+
+
+
+final class TestClass
+extends TestParentClass
+implements TestInterface
+{
+
+	static public function staticMethod(string $a, string $b, string $c) : string {
+		return implode('/', [$a, $b, $c]);
+	}
+
+
+
+	public $foo;
+	public $bar;
+	public $baz;
+
+
+	public function __construct(string $a, string $b, string $c) {
+		$this->foo = $a;
+		$this->bar = $b;
+		$this->baz = $c;
+	}
+
+
+	public function memberMethod(string $a, string $b, string $c = '4') : string {
+		return self::staticMethod($a, $b, $c);
+	}
+
+	public function thisMethod(string $a, string $b) : string {
+		return self::staticMethod($a, $b, $this->baz);
+	}
+}
---
+++ test/factory/TestInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace test\factory;
+
+
+
+interface TestInterface
+{
+
+}
