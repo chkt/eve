c0c90cf228916a36163afda8fd64d959d40d3b95
chkt <ck@chkt.one>
2017-08-15 18:50:19 +0200

    Add ReferenceResolver

---
+++ source/inject/resolve/ReferenceResolver.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace eve\inject\resolve;
+
+use eve\access\ITraversableAccessor;
+use eve\inject\IInjector;
+
+
+
+final class ReferenceResolver
+implements IInjectorResolver
+{
+
+	static public function getDependencyConfig(ITraversableAccessor $config) : array {
+		return [[
+			'type' => IInjector::TYPE_ARGUMENT,
+			'data' => $config->getItem('driver')->getReferences()
+		]];
+	}
+
+
+
+	private $_refs;
+
+
+	public function __construct(ITraversableAccessor $references) {
+		$this->_refs = $references;
+	}
+
+
+	public function produce(ITraversableAccessor $config) {
+		return $this->_refs->getItem($config->getItem('name'));
+	}
+}
