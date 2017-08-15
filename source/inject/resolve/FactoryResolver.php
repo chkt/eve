9fb2272a435b2e58f903c24e2b25e9032d7be570
chkt <ck@chkt.one>
2017-08-15 18:53:15 +0200

    Add FactoryResolver

---
+++ source/inject/resolve/FactoryResolver.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace eve\inject\resolve;
+
+use eve\access\ITraversableAccessor;
+use eve\inject\IInjectableFactory;
+use eve\inject\IInjector;
+
+
+
+final class FactoryResolver
+implements IInjectorResolver
+{
+
+	static public function getDependencyConfig(ITraversableAccessor $config) : array {
+		return [[
+			'type' => IInjector::TYPE_ARGUMENT,
+			'data' => $config->getItem('driver')->getInjector()
+		]];
+	}
+
+
+
+	private $_injector;
+
+
+	public function __construct(IInjector $injector) {
+		$this->_injector = $injector;
+	}
+
+
+	public function produce(ITraversableAccessor $config) {
+		$fab = $this->_injector->produce($config->getItem('factory'));
+
+		if (!($fab instanceof IInjectableFactory)) throw new \ErrorException(sprintf('INJ not a factory "%s"', $config->getItem('factory')));
+
+		return $fab->produce($config);
+	}
+}
