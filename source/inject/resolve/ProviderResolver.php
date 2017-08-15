3c2c9fe156300b30f49d20d20f434bd5cd11423e
chkt <ck@chkt.one>
2017-08-15 18:49:34 +0200

    Add ProviderResolver

---
+++ source/inject/resolve/ProviderResolver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace eve\inject\resolve;
+
+use eve\access\ITraversableAccessor;
+use eve\inject\IInjector;
+use eve\provide\ILocator;
+
+
+
+final class ProviderResolver
+implements IInjectorResolver
+{
+
+	static public function getDependencyConfig(ITraversableAccessor $config) : array {
+		return [[
+			'type' => IInjector::TYPE_ARGUMENT,
+			'data' => $config->getItem('driver')->getLocator()
+		]];
+	}
+
+
+
+	private $_locator;
+
+
+	public function __construct(ILocator $locator) {
+		$this->_locator = $locator;
+	}
+
+
+	public function produce(ITraversableAccessor $config) {
+		return $this->_locator
+			->getItem($config->getItem('type'))
+			->getItem($config->getItem('location'));
+	}
+}
