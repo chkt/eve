8696152c377b02ae4c7d8bc5d976fe09161da2f3
chkt <ck@chkt.one>
2017-08-25 12:25:34 +0200

    Add IdentityInjectorTest

---
+++ test/inject/IdentityInjectorTest.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace test\inject;
+
+use PHPUnit\Framework\TestCase;
+
+use eve\common\ISimpleFactory;
+use eve\access\IItemMutator;
+use eve\access\TraversableAccessor;
+use eve\access\TraversableMutator;
+use eve\factory\IFactory;
+use eve\inject\IInjectable;
+use eve\inject\IInjectableIdentity;
+use eve\inject\Injector;
+use eve\inject\IdentityInjector;
+use eve\driver\IInjectorDriver;
+
+
+
+final class IdentityInjectorTest
+extends TestCase
+{
+
+	private function _mockFactory() : IFactory {
+		$ins = $this
+			->getMockBuilder(IFactory::class)
+			->getMock();
+
+		$ins
+			->expects($this->any())
+			->method('hasInterface')
+			->with(
+				$this->logicalAnd(
+					$this->isType('string'),
+					$this->logicalNot($this->isEmpty())
+				),
+				$this->isType('string')
+			)
+			->willReturnCallback(function(string $qname, string $iname) : bool {
+				switch ($qname) {
+					case IInjectableIdentity::class : return $iname === IInjectableIdentity::class || $iname === IInjectable::class;
+					case IInjectable::class : return $iname === $qname;
+					default : return false;
+				}
+			});
+
+		$ins
+			->expects($this->any())
+			->method('callMethod')
+			->with(
+				$this->isType('string'),
+				$this->logicalOr(
+					$this->equalTo('getDependencyConfig'),
+					$this->equalTo('getInstanceIdentity')
+				),
+				$this->isType('array')
+			)
+			->willReturnCallback(function(string $qname, string $method, array $args) {
+				switch ($method) {
+					case 'getDependencyConfig' : return [];
+					case 'getInstanceIdentity' : return implode('-', $args[0]->getProjection());
+					default : throw new \ErrorException();
+				}
+			});
+
+		$ins
+			->expects($this->any())
+			->method('newInstance')
+			->with(
+				$this->logicalAnd(
+					$this->isType('string'),
+					$this->logicalNot($this->isEmpty())
+				),
+				$this->isType('array')
+			)
+			->willReturnCallback(function(string $qname, array $args) {
+				return $this
+					->getMockBuilder($qname)
+					->getMock();
+			});
+
+		return $ins;
+	}
+
+	private function _mockAccessorFactory() : ISimpleFactory {
+		$ins = $this
+			->getMockBuilder(ISimpleFactory::class)
+			->getMock();
+
+		$ins
+			->expects($this->any())
+			->method('instance')
+			->with($this->isType('array'))
+			->willReturnCallback(function(array& $data) {
+				return new TraversableAccessor($data);
+			});
+
+		return $ins;
+	}
+
+	private function _mockDriver(IItemMutator $cache = null) : IInjectorDriver {
+		$factory = $this->_mockFactory();
+		$accessor = $this->_mockAccessorFactory();
+
+		if (is_null($cache)) $cache = $this->_produceCache();
+
+		$ins = $this
+			->getMockBuilder(IInjectorDriver::class)
+			->getMock();
+
+		$ins
+			->expects($this->exactly(2))
+			->method('getFactory')
+			->with()
+			->willReturn($factory);
+
+		$ins
+			->expects($this->exactly(2))
+			->method('getAccessorFactory')
+			->with()
+			->willReturn($accessor);
+
+		$ins
+			->expects($this->once())
+			->method('getInstanceCache')
+			->with()
+			->willReturn($cache);
+
+		return $ins;
+	}
+
+
+	private function _produceCache() {
+		$data = [];
+
+		return new TraversableMutator($data);
+	}
+
+
+	private function _produceInjector(IInjectorDriver $driver = null) {
+		if (is_null($driver)) $driver = $this->_mockDriver();
+
+		return new IdentityInjector($driver, []);
+	}
+
+
+	public function testInheritance() {
+		$injector = $this->_produceInjector();
+
+		$this->assertInstanceOf(Injector::class, $injector);
+	}
+
+	public function testProduce() {
+		$cache = $this->_produceCache();
+		$driver = $this->_mockDriver($cache);
+		$injector = $this->_produceInjector($driver);
+
+		$a =  $injector->produce(IInjectableIdentity::class, [
+			'foo',
+			'bar'
+		]);
+
+		$this->assertInstanceOf(IInjectableIdentity::class, $a);
+		$this->assertTrue($cache->hasKey(IInjectableIdentity::class . ':foo-bar'));
+		$this->assertSame($a, $cache->getItem(IInjectableIdentity::class . ':foo-bar'));
+
+		$b = $injector->produce(IInjectableIdentity::class, [
+			'foo',
+			'bar'
+		]);
+
+		$this->assertSame($a, $b);
+
+		$c = $injector->produce(IInjectableIdentity::class, [
+			'foo',
+			'baz'
+		]);
+
+		$this->assertInstanceOf(IInjectableIdentity::class, $c);
+		$this->assertNotSame($c, $a);
+		$this->assertTrue($cache->hasKey(IInjectableIdentity::class . ':foo-baz'));
+		$this->assertSame($c, $cache->getItem(IInjectableIdentity::class . ':foo-baz'));
+	}
+
+	public function testProduce_noIdentity() {
+		$cache = $this->_produceCache();
+		$driver = $this->_mockDriver($cache);
+		$injector = $this->_produceInjector($driver);
+
+		$a = $injector->produce(IInjectable::class, [
+			'foo',
+			'bar'
+		]);
+
+		$this->assertInstanceOf(IInjectable::class, $a);
+		$this->assertFalse($cache->hasKey(IInjectable::class . ':foo-bar'));
+
+		$b = $injector->produce(IInjectable::class, [
+			'foo',
+			'bar'
+		]);
+
+		$this->assertInstanceOf(IInjectable::class, $b);
+		$this->assertNotSame($a, $b);
+	}
+}
