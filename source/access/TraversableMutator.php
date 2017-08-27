92a9c7da8d148bb482fe4660d77349832c58e2ae
chkt <ck@chkt.one>
2017-08-27 12:51:48 +0200

    Change ->removeKey() and ->setItem() to use ->_useData()

--- source/access/TraversableMutator.php
+++ source/access/TraversableMutator.php
@@ -9,24 +9,16 @@ extends TraversableAccessor
 implements IItemMutator
 {
 
-	private $_data;
-
-
-	public function __construct(array& $data) {
-		parent::__construct($data);
-
-		$this->_data =& $data;
-	}
-
-
 	public function removeKey(string $key) : IKeyMutator {
-		if (array_key_exists($key, $this->_data)) unset($this->_data[$key]);
+		$data =& $this->_useData();
+
+		if (array_key_exists($key, $data)) unset($data[$key]);
 
 		return $this;
 	}
 
 	public function setItem(string $id, $item) : IItemMutator {
-		$this->_data[$id] = $item;
+		$this->_useData()[$id] = $item;
 
 		return $this;
 	}
