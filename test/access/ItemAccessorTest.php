7cbec5a2970fd7470b232fc842e84c6f8485efe2
chkt <ck@chkt.one>
2017-09-02 12:42:10 +0200

    Add ->testGetItem_handleNoKey(); Update ->testGetItem_noKey() to expect AccessorException

--- test/access/ItemAccessorTest.php
+++ test/access/ItemAccessorTest.php
@@ -2,6 +2,7 @@
 
 namespace test\access;
 
+use eve\access\AccessorException;
 use PHPUnit\Framework\TestCase;
 
 use eve\access\IKeyAccessor;
@@ -13,6 +14,19 @@ use eve\access\ItemAccessor;
 final class ItemAccessorTest
 extends TestCase
 {
+
+	private function _mockAccessor(array& $data = null) {
+		if (is_null($data)) $data = $this->_produceSampleData();
+
+		$ins = $this
+			->getMockBuilder(ItemAccessor::class)
+			->setConstructorArgs([ & $data])
+			->setMethods(['_handleAccessorException'])
+			->getMock();
+
+		return $ins;
+	}
+
 
 	private function _produceSampleData() {
 		return [
@@ -65,10 +79,29 @@ extends TestCase
 	public function testGetItem_noKey() {
 		$ins = $this->_produceAccessor();
 
-		$this->expectException(\ErrorException::class);
+		$this->expectException(AccessorException::class);
 		$this->expectExceptionMessage(sprintf('ACC invalid key "baz"'));
 
 		$ins->getItem('baz');
+	}
+
+	public function testGetItem_handleNoKey() {
+		$data = $this->_produceSampleData();
+		$ins = $this->_mockAccessor($data);
+
+		$ins
+			->expects($this->once())
+			->method('_handleAccessorException')
+			->with($this->isInstanceOf(AccessorException::class))
+			->willReturnCallback(function(AccessorException $ex) use (& $data) : bool {
+				$this->assertEquals('baz', $ex->getKey());
+
+				$data['baz'] = 3;
+
+				return true;
+			});
+
+		$this->assertEquals(3, $ins->getItem('baz'));
 	}
 
 
