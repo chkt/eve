fad4ded05d98ab5f844e6819e38034803a2a361b
chkt <ck@chkt.one>
2017-09-02 12:41:21 +0200

    Add AccessorException

---
+++ source/access/AccessorException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace eve\access;
+
+
+
+final class AccessorException
+extends \Exception
+implements IAccessorException
+{
+
+	private $_key;
+
+
+	public function __construct(string $key) {
+		parent::__construct(sprintf('ACC invalid key "%s"', $key));
+
+		$this->_key = $key;
+	}
+
+
+	public function getKey() : string {
+		return $this->_key;
+	}
+}
