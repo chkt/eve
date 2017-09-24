f068c17bca09fc274329318035443575eb569ea6
chkt <ck@chkt.one>
2017-09-24 20:39:52 +0200

    Add ::IDENTITY_SINGLE, ::IDENTITY_DEFAULT

--- source/inject/IInjectableIdentity.php
+++ source/inject/IInjectableIdentity.php
@@ -10,5 +10,10 @@ interface IInjectableIdentity
 extends IInjectable
 {
 
+	const IDENTITY_SINGLE = 'one';
+	const IDENTITY_DEFAULT = 'default';
+
+
+
 	static public function getInstanceIdentity(ITraversableAccessor $config) : string;
 }
