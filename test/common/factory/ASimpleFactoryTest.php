f62d55ceefab7a6672335e89853c06603805b40d
chkt <ck@chkt.one>
2018-01-25 14:39:49 +0800

    Update ASimpleFactoryTest to test for non-overridden config defaults, supply ICoreFactory

--- test/common/factory/ASimpleFactoryTest.php
+++ test/common/factory/ASimpleFactoryTest.php
@@ -5,6 +5,7 @@ namespace test\common\factory;
 use PHPUnit\Framework\TestCase;
 
 use eve\common\IFactory;
+use eve\common\factory\ICoreFactory;
 use eve\common\factory\ISimpleFactory;
 use eve\common\factory\ASimpleFactory;
 
@@ -14,30 +15,46 @@ final class ASimpleFactoryTest
 extends TestCase
 {
 
-	private function _mockFactory(array $defaults = [], callable $fn) {
+	private function _mockCoreFactory() {
+		return $this
+			->getMockBuilder(ICoreFactory::class)
+			->getMock();
+	}
+
+	private function _mockFactory(callable $fn, array $config = null, ICoreFactory $core = null) {
+		if (is_null($core)) $core = $this->_mockCoreFactory();
+
+		$methods = ['_produceInstance'];
+
+		if (!is_null($config)) $methods[] = '_getConfigDefaults';
+
 		$ins = $this
 			->getMockBuilder(ASimpleFactory::class)
-			->setMethods(['_getConfigDefaults', '_produceInstance'])
+			->setConstructorArgs([$core])
+			->setMethods($methods)
 			->getMockForAbstractClass();
 
 		$ins
 			->expects($this->any())
+			->method('_produceInstance')
+			->with(
+				$this->isInstanceOf(ICoreFactory::class),
+				$this->isType('array')
+			)
+			->willReturnCallback($fn);
+
+		if (!is_null($config)) $ins
+			->expects($this->any())
 			->method('_getConfigDefaults')
 			->with()
-			->willReturn($defaults);
-
-		$ins
-			->expects($this->any())
-			->method('_produceInstance')
-			->with($this->isType('array'))
-			->willReturnCallback($fn);
+			->willReturn($config);
 
 		return $ins;
 	}
 
 
 	public function testInheritance() {
-		$factory = $this->_mockFactory([], function() {});
+		$factory = $this->_mockFactory(function() {});
 
 		$this->assertInstanceOf(ISimpleFactory::class, $factory);
 		$this->assertInstanceOf(IFactory::class, $factory);
@@ -90,12 +107,19 @@ extends TestCase
 			]
 		];
 
-		$factory = $this->_mockFactory($a, function(array $config) use ($c) {
-			$this->assertEquals($c, $config);
+		$factoryA = $this->_mockFactory(function(ICoreFactory $core, array $config) use ($b) {
+			$this->assertEquals($b, $config);
 
 			return 'foo';
 		});
 
-		$this->assertEquals('foo', $factory->produce($b));
+		$factoryB = $this->_mockFactory(function(ICoreFactory $core, array $config) use ($c) {
+			$this->assertEquals($c, $config);
+
+			return 'bar';
+		}, $a);
+
+		$this->assertEquals('foo', $factoryA->produce($b));
+		$this->assertEquals('bar', $factoryB->produce($b));
 	}
 }
