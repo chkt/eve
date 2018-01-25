e0fb7b1faef288daae3ad7354f8aee02b84271df
chkt <ck@chkt.one>
2018-01-25 14:43:21 +0800

    Update InjectorDriverFactoryTest

--- test/driver/InjectorDriverFactoryTest.php
+++ test/driver/InjectorDriverFactoryTest.php
@@ -5,15 +5,15 @@ namespace test\driver;
 use PHPUnit\Framework\TestCase;
 
 use eve\common\factory\ISimpleFactory;
-use eve\common\factory\ASimpleFactory;
 use eve\common\factory\ICoreFactory;
+use eve\common\factory\ASimpleFactory;
 use eve\access\ITraversableAccessor;
 use eve\access\IItemMutator;
 use eve\access\TraversableAccessor;
 use eve\entity\IEntityParser;
 use eve\inject\IInjector;
 use eve\provide\ILocator;
-use eve\driver\IInjectorDriver;
+use eve\driver\InjectorDriver;
 use eve\driver\InjectorDriverFactory;
 
 
@@ -22,33 +22,25 @@ final class InjectorDriverFactoryTest
 extends TestCase
 {
 
-	private function _mockInterface(string $qname) {
-		return $this
+	private function _mockInterface(string $qname, array $args = []) {
+		$ins = $this
 			->getMockBuilder($qname)
 			->getMock();
-	}
 
-	private function _mockFactory() : ICoreFactory {
-		$ins = $this
-			->getMockBuilder(ICoreFactory::class)
-			->getMock();
+		foreach ($args as $key => $value) {
+			$prop = (is_numeric($key) ? 'p' : '') . $key;
 
-		$ins
-			->expects($this->once())
-			->method('newInstance')
-			->with($this->equalTo(\eve\driver\InjectorDriver::class))
-			->willReturnCallback(function(string $qname, array $args) {
-				return new $qname(...$args);
-			});
+			$ins->$prop = $value;
+		}
 
 		return $ins;
 	}
+
 
 	private function _mockAccessorFactory() : ISimpleFactory {
 		$ins = $this
 			->getMockBuilder(ISimpleFactory::class)
 			->getMock();
-
 
 		$ins
 			->expects($this->any())
@@ -62,54 +54,128 @@ extends TestCase
 	}
 
 
-	private function _produceFactory() {
-		return new InjectorDriverFactory();
+	private function _produceDriverFactory(ICoreFactory $core) {
+		return new InjectorDriverFactory($core);
 	}
 
 
 	public function testInheritance() {
-		$fab = $this->_produceFactory();
+		$core = $this->_mockInterface(ICoreFactory::class);
+		$fab = $this->_produceDriverFactory($core);
 
 		$this->assertInstanceOf(ASimpleFactory::class, $fab);
 	}
 
-	public function testProduce() {
-		$driver = $this
-			->_produceFactory()
-			->produce();
+	public function testProduce_names() {
+		$core = $this
+			->getMockBuilder(ICoreFactory::class)
+			->getMock();
 
-		$this->assertInstanceOf(IInjectorDriver::class, $driver);
-		$this->assertInstanceOf(ICoreFactory::class, $driver->getCoreFactory());
+		$core
+			->expects($this->any())
+			->method('newInstance')
+			->with(
+				$this->isType('string'),
+				$this->logicalOr(
+					$this->isType('array'),
+					$this->isNull()
+				)
+			)
+			->willReturnCallback(function(string $qname, array $args = []) {
+				if ($qname === InjectorDriver::class) return new InjectorDriver(...$args);
+				else if ($qname === ISimpleFactory::class) return $this->_mockAccessorFactory();
+				else if ($qname === IInjector::class) {
+					$injector = $this->_mockInterface(IInjector::class, $args);
+
+					$injector
+						->expects($this->once())
+						->method('produce')
+						->with(
+							$this->equalTo(ILocator::class),
+							$this->isType('array')
+						)
+						->willReturnCallback(function($qname, array $args = []) {
+							return $this->_mockInterface($qname, $args);
+						});
+
+					return $injector;
+				}
+				else return $this->_mockInterface($qname, $args);
+			});
+
+		$fab = $this->_produceDriverFactory($core);
+
+		$config = [
+			'accessorFactoryName' => ISimpleFactory::class,
+			'instanceCacheName' => IItemMutator::class,
+			'entityParserName' => IEntityParser::class,
+			'injectorName' => IInjector::class,
+			'locatorName' => ILocator::class,
+			'providers' => [
+				'foo' => 'bar',
+				'baz' => 'quux'
+			]
+		];
+
+		$driver = $fab->produce($config);
+
+		$this->assertInstanceOf(InjectorDriver::class, $driver);
 		$this->assertInstanceOf(ISimpleFactory::class, $driver->getAccessorFactory());
-		$this->assertInstanceOf(IEntityParser::class, $driver->getEntityParser());
 		$this->assertInstanceOf(ITraversableAccessor::class, $driver->getReferences());
+		$this->assertInstanceOf(IItemMutator::class, $driver->getInstanceCache());
+		$this->assertInstanceOf(IEntityParser::class, $driver->getEntityParser());
 		$this->assertInstanceOf(IInjector::class, $driver->getInjector());
+		$this->assertSame($driver, $driver->getInjector()->p0);
+		$this->assertInternalType('array', $driver->getInjector()->p1);
 		$this->assertInstanceOf(ILocator::class, $driver->getLocator());
+		$this->assertSame($driver, $driver->getLocator()->driver);
+		$this->assertSame($config['providers'], $driver->getLocator()->providerNames);
 	}
 
 	public function testProduce_instances() {
-		$factory = $this->_mockFactory();
-		$accessor = $this->_mockAccessorFactory();
-		$parser = $this->_mockInterface(IEntityParser::class);
+		$core = $this
+			->getMockBuilder(ICoreFactory::class)
+			->getMock();
+
+		$core
+			->expects($this->once())
+			->method('newInstance')
+			->with($this->equalTo(InjectorDriver::class))
+			->willReturnCallback(function(string $qname, array $args) {
+				return new InjectorDriver(...$args);
+			});
+
+		$accessor = $this
+			->getMockBuilder(ISimpleFactory::class)
+			->getMock();
+
+		$accessor
+			->expects($this->any())
+			->method('produce')
+			->with($this->isType('array'))
+			->willReturnCallback(function(array& $data) {
+				return new TraversableAccessor($data);
+			});
+
 		$cache = $this->_mockInterface(IItemMutator::class);
+		$parser = $this->_mockInterface(IEntityParser::class);
 		$injector = $this->_mockInterface(IInjector::class);
 		$locator = $this->_mockInterface(ILocator::class);
+		$fab = $this->_produceDriverFactory($core);
 
 		$config = [
-			'coreFactory' => $factory,
 			'accessorFactory' => $accessor,
-			'entityParser' => $parser,
 			'instanceCache' => $cache,
+			'entityParser' => $parser,
 			'injector' => $injector,
 			'locator' => $locator
 		];
 
-		$driver = $this
-			->_produceFactory()
-			->produce($config);
+		$driver = $fab->produce($config);
 
-		$this->assertSame($factory, $driver->getCoreFactory());
+		$this->assertInstanceOf(InjectorDriver::class, $driver);
 		$this->assertSame($accessor, $driver->getAccessorFactory());
+		$this->assertSame($cache, $driver->getInstanceCache());
 		$this->assertSame($parser, $driver->getEntityParser());
 		$this->assertSame($injector, $driver->getInjector());
 		$this->assertSame($locator, $driver->getLocator());
