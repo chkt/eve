803cf7c373356d140a94acb5abb97470a707de36
chkt <ck@chkt.one>
2018-02-26 12:54:19 +0800

    Add AItemAccessorComposition

---
+++ source/common/access/operator/AItemAccessorComposition.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace eve\common\access\operator;
+
+use eve\common\factory\ISimpleFactory;
+use eve\common\projection\IProjectable;
+use eve\common\projection\operator\AProjectableSurrogate;
+use eve\common\access\IItemAccessor;
+use eve\common\access\AccessorException;
+
+
+
+abstract class AItemAccessorComposition
+extends AProjectableSurrogate
+implements ISimpleFactory, IItemAccessorComposition
+{
+
+	public function select(IProjectable $source, string $key) : IItemAccessor {
+		$data = $source->getProjection();
+
+		if (!array_key_exists($key, $data) || !is_array($data[$key])) throw new AccessorException($key);
+
+		return $this->produce($data[$key]);
+	}
+}
