d2de5e0fec00c15dadf1bf653131c91118a25f2d
chkt <ck@chkt.one>
2018-02-26 13:31:59 +0800

    Add AAssemblyHost

---
+++ source/common/assembly/AAssemblyHost.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace eve\common\assembly;
+
+use eve\common\access\ItemAccessor;
+use eve\common\access\AccessorException;
+use eve\common\assembly\exception\InvalidKeyException;
+use eve\common\assembly\exception\DependencyLoopException;
+
+
+
+abstract class AAssemblyHost
+extends ItemAccessor
+implements IAssemblyHost
+{
+
+	private $_stack;
+
+
+	public function __construct(array& $data = []) {
+		parent::__construct($data);
+
+		$this->_stack = [];
+	}
+
+
+	abstract protected function _produceItem(string $key);
+
+
+	protected function _handleAccessorException(AccessorException $ex) : bool {
+		$key = $ex->getKey();
+
+		if (in_array($key, $this->_stack)) throw new DependencyLoopException($key, $ex);
+
+		array_push($this->_stack, $key);
+
+		$this->_useData()[$key] = $this->_produceItem($key);
+
+		array_pop($this->_stack);
+
+		return true;
+	}
+
+
+	public function hasKey(string $key) : bool {
+		try {
+			return parent::hasKey($key) || !is_null($this->getItem($key));
+		}
+		catch (InvalidKeyException $ex){
+			return false;
+		}
+	}
+}
