e5a8dc111a37a7c39b252c9d607404c55061e35e
chkt <ck@chkt.one>
2018-02-26 13:38:40 +0800

    Remove ItemAccessorDependency Add IAssemblyHost argument to constructor

--- source/driver/InjectorDriver.php
+++ source/driver/InjectorDriver.php
@@ -2,50 +2,74 @@
 
 namespace eve\driver;
 
+use eve\common\assembly\IAssemblyHost;
 use eve\common\factory\ISimpleFactory;
 use eve\common\factory\ICoreFactory;
 use eve\common\access\IItemMutator;
-use eve\common\access\ItemAccessor;
-use eve\entity\IEntityParser;
+use eve\common\access\AccessorException;
 use eve\inject\IInjector;
 use eve\inject\cache\IKeyEncoder;
 use eve\provide\ILocator;
+use eve\entity\IEntityParser;
 
 
 
 class InjectorDriver
-extends ItemAccessor
 implements IInjectorDriver
 {
 
-	public function getInjector() : IInjector {
-		return $this->getItem(self::ITEM_INJECTOR);
+	private $_assembly;
+
+
+	public function __construct(IAssemblyHost $assembly) {
+		$this->_assembly = $assembly;
 	}
 
-	public function getLocator() : ILocator {
-		return $this->getItem(self::ITEM_LOCATOR);
+
+	public function hasKey(string $key) : bool {
+		return in_array($key, [
+			self::ITEM_CORE_FACTORY,
+			self::ITEM_ACCESSOR_FACTORY,
+			self::ITEM_KEY_ENCODER,
+			self::ITEM_INSTANCE_CACHE,
+			self::ITEM_INJECTOR,
+			self::ITEM_ENTITY_PARSER,
+			self::ITEM_LOCATOR
+		]);
 	}
 
+	public function getItem(string $key) {
+		if (!$this->hasKey($key)) throw new AccessorException($key);
 
-	public function getEntityParser() : IEntityParser {
-		return $this->getItem(self::ITEM_ENTITY_PARSER);
+		return $this->_assembly->getItem($key);
 	}
 
 
 	public function getCoreFactory() : ICoreFactory {
-		return $this->getItem(self::ITEM_CORE_FACTORY);
+		return $this->_assembly->getItem(self::ITEM_CORE_FACTORY);
 	}
 
 	public function getAccessorFactory() : ISimpleFactory {
-		return $this->getItem(self::ITEM_ACCESSOR_FACTORY);
+		return $this->_assembly->getItem(self::ITEM_ACCESSOR_FACTORY);
 	}
 
-
 	public function getKeyEncoder() : IKeyEncoder {
-		return $this->getItem(self::ITEM_KEY_ENCODER);
+		return $this->_assembly->getItem(self::ITEM_KEY_ENCODER);
 	}
 
 	public function getInstanceCache() : IItemMutator {
-		return $this->getItem(self::ITEM_INSTANCE_CACHE);
+		return $this->_assembly->getItem(self::ITEM_INSTANCE_CACHE);
+	}
+
+	public function getInjector() : IInjector {
+		return $this->_assembly->getItem(self::ITEM_INJECTOR);
+	}
+
+	public function getEntityParser() : IEntityParser {
+		return $this->_assembly->getItem(self::ITEM_ENTITY_PARSER);
+	}
+
+	public function getLocator() : ILocator {
+		return $this->_assembly->getItem(self::ITEM_LOCATOR);
 	}
 }
