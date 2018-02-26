29e24609d8d6193a6477a860608ce76afa7d8650
chkt <ck@chkt.one>
2018-02-26 13:10:40 +0800

    Update TraversableAccessorFactory to inherit AItemAccessorComposition

--- source/common/access/TraversableAccessorFactory.php
+++ source/common/access/TraversableAccessorFactory.php
@@ -2,24 +2,24 @@
 
 namespace eve\common\access;
 
-use eve\common\factory\ISimpleFactory;
 use eve\common\factory\ICoreFactory;
+use eve\common\access\operator\AItemAccessorComposition;
 
 
 
 final class TraversableAccessorFactory
-implements ISimpleFactory
+extends AItemAccessorComposition
 {
 
-	private $_fab;
+	private $_baseFactory;
 
 
-	public function __construct(ICoreFactory $fab) {
-		$this->_fab = $fab;
+	public function __construct(ICoreFactory $baseFactory) {
+		$this->_baseFactory = $baseFactory;
 	}
 
 
 	public function produce(array& $data) {
-		return $this->_fab->newInstance(TraversableAccessor::class, [ & $data ]);
+		return $this->_baseFactory->newInstance(TraversableAccessor::class, [ & $data ]);
 	}
 }
