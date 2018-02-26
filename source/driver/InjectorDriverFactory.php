956bfb6a1decad144790541c7d96f93ecd1cdde3
chkt <ck@chkt.one>
2018-02-26 13:44:21 +0800

    Update to create InjectorDriverAssembly instead of dependencies

--- source/driver/InjectorDriverFactory.php
+++ source/driver/InjectorDriverFactory.php
@@ -2,21 +2,12 @@
 
 namespace eve\driver;
 
-use eve\common\factory\ISimpleFactory;
 use eve\common\factory\ICoreFactory;
+use eve\common\factory\ISimpleFactory;
 use eve\common\factory\ASimpleFactory;
 use eve\common\access\ITraversableAccessor;
-use eve\common\access\IItemMutator;
-use eve\common\access\TraversableMutator;
-use eve\common\access\TraversableAccessorFactory;
-use eve\entity\IEntityParser;
-use eve\entity\EntityParser;
+use eve\common\assembly\IAssemblyHost;
 use eve\inject\IInjector;
-use eve\inject\IdentityInjector;
-use eve\inject\cache\IKeyEncoder;
-use eve\inject\cache\KeyEncoder;
-use eve\provide\ILocator;
-use eve\provide\ProviderProvider;
 
 
 
@@ -38,59 +29,32 @@ extends ASimpleFactory
 
 
 	protected function _produceAccessorFactory(ICoreFactory $base, array $config) : ISimpleFactory {
-		return $base->newInstance(TraversableAccessorFactory::class);
+		return $base->newInstance(\eve\common\access\TraversableAccessorFactory::class);
 	}
 
-	protected function _produceDriver(ICoreFactory $base, ITraversableAccessor $config, array& $dependencies) : IInjectorDriver {
-		return $base->newInstance(InjectorDriver::class, [ & $dependencies ]);
-	}
-
-	protected function _produceKeyEncoder(IInjectorDriver $driver, ITraversableAccessor $config) : IKeyEncoder {
-		$base = $driver->getCoreFactory();
-
-		return $base->newInstance(KeyEncoder::class, [ $base ]);
-	}
-
-	protected function _produceInstanceCache(IInjectorDriver $driver, ITraversableAccessor $config) : IItemMutator {
-		return $driver->getCoreFactory()->newInstance(TraversableMutator::class, [ [] ]);
-	}
-
-	protected function _produceEntityParser(IInjectorDriver $driver, ITraversableAccessor $config) : IEntityParser {
-		return $driver->getCoreFactory()->newInstance(EntityParser::class);
-	}
-
-	protected function _produceInjector(IInjectorDriver $driver, ITraversableAccessor $config) : IInjector {
-		return $driver->getCoreFactory()->newInstance(IdentityInjector::class, [
-			$driver,
-			$config->getItem('resolvers')
+	protected function _produceAssembly(ICoreFactory $base, ISimpleFactory $access, ITraversableAccessor $config) : IAssemblyHost {
+		return $base->newInstance(\eve\driver\InjectorDriverAssembly::class, [
+			$base,
+			$access,
+			$config
 		]);
 	}
 
-	protected function _produceLocator(IInjectorDriver $driver, ITraversableAccessor $config) : ILocator {
-		return $driver->getInjector()->produce(ProviderProvider::class, [
-			'driver' => $driver,
-			'providerNames' => $config->getItem('providers')
-		]);
+	protected function _produceDriver(IAssemblyHost $assembly) : IInjectorDriver {
+		return $assembly
+			->getItem('coreFactory')
+			->newInstance(\eve\driver\InjectorDriver::class, [
+				$assembly
+			]);
 	}
 
 
 	protected function _produceInstance(ICoreFactory $base, array $config) {
 		$access = $this->_produceAccessorFactory($base, $config);
-
-		$deps = [
-			IInjectorDriver::ITEM_CORE_FACTORY => $base,
-			IInjectorDriver::ITEM_ACCESSOR_FACTORY => $access
-		];
-
 		$data = $access->produce($config);
-		$driver = $this->_produceDriver($base, $data, $deps);
 
-		$deps[IInjectorDriver::ITEM_KEY_ENCODER] = $this->_produceKeyEncoder($driver, $data);
-		$deps[IInjectorDriver::ITEM_INSTANCE_CACHE] = $this->_produceInstanceCache($driver, $data);
-		$deps[IInjectorDriver::ITEM_ENTITY_PARSER] = $this->_produceEntityParser($driver, $data);
-		$deps[IInjectorDriver::ITEM_INJECTOR] = $this->_produceInjector($driver, $data);
-		$deps[IInjectorDriver::ITEM_LOCATOR] = $this->_produceLocator($driver, $data);
+		$assembly = $this->_produceAssembly($base, $access, $data);
 
-		return $driver;
+		return $this->_produceDriver($assembly);
 	}
 }
