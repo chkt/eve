768d87b139514f6426404a055e483c0fc14ca63a
chkt <ck@chkt.one>
2018-05-03 19:37:11 +0800

    Change to extend IMethodProxy

--- source/common/factory/ICoreFactory.php
+++ source/common/factory/ICoreFactory.php
@@ -3,18 +3,15 @@
 namespace eve\common\factory;
 
 use eve\common\IFactory;
+use eve\common\base\IMethodProxy;
 
 
 
 interface ICoreFactory
-extends IFactory
+extends IFactory, IMethodProxy
 {
 
 	public function hasInterface(string $qname, string $iname) : bool;
 
-
 	public function newInstance(string $qname, array $args = []);
-
-
-	public function callMethod(string $qname, string $method, array $args = []);
 }
