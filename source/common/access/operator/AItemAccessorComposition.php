cc3e4a867086bc4c6f663a6fdecfbc7bca50859c
chkt <ck@chkt.one>
2018-05-03 19:46:09 +0800

    Update ->select() to collate selector to $source

--- source/common/access/operator/AItemAccessorComposition.php
+++ source/common/access/operator/AItemAccessorComposition.php
@@ -3,23 +3,22 @@
 namespace eve\common\access\operator;
 
 use eve\common\factory\ISimpleFactory;
-use eve\common\projection\IProjectable;
 use eve\common\projection\operator\AProjectableSurrogate;
 use eve\common\access\IItemAccessor;
 use eve\common\access\exception\AccessorException;
 
 
 
-abstract class AItemAccessorComposition
+abstract class AItemAccessorComposition		//TODO: misnamed, should be AItemAccessorSurrogate
 extends AProjectableSurrogate
 implements ISimpleFactory, IItemAccessorComposition
 {
 
-	public function select(IProjectable $source, string $key) : IItemAccessor {
-		$data = $source->getProjection();
+	public function select(IItemAccessor $source, string $key) : IItemAccessor {
+		$item = $source->getItem($key);
 
-		if (!array_key_exists($key, $data) || !is_array($data[$key])) throw new AccessorException($key);
+		if (!is_array($item)) throw new AccessorException($key);
 
-		return $this->produce($data[$key]);
+		return $this->produce($item);
 	}
 }
