c2501e9b83c913f7a004231b31d02b51bdd0cf2e
chkt <ck@chkt.one>
2018-05-12 15:37:02 +0800

    Fix configuration passing in ->produce()

--- source/inject/resolve/FactoryResolver.php
+++ source/inject/resolve/FactoryResolver.php
@@ -3,6 +3,7 @@
 namespace eve\inject\resolve;
 
 use eve\common\access\ITraversableAccessor;
+use eve\common\assembly\IAssemblyHost;
 use eve\inject\IInjectableFactory;
 use eve\inject\IInjector;
 
@@ -15,27 +16,37 @@ implements IInjectorResolver
 	static public function getDependencyConfig(ITraversableAccessor $config) : array {
 		return [[
 			'type' => IInjector::TYPE_ARGUMENT,
-			'data' => $config
-				->getItem('driver')
-				->getItem('injector')
+			'data' => $config->getItem('driver')
 		]];
 	}
 
 
 
-	private $_injector;
+	private $_driver;
 
 
-	public function __construct(IInjector $injector) {
-		$this->_injector = $injector;
+	public function __construct(IAssemblyHost $driver) {
+		$this->_driver = $driver;
+	}
+
+
+	private function _getInstanceConfig(ITraversableAccessor $data) : ITraversableAccessor {
+		$accessor = $this->_driver->getItem('accessorFactory');
+
+		if ($data->hasKey('config')) return $accessor->select($data, 'config');
+		else return $accessor->produce(($data = []));
 	}
 
 
 	public function produce(ITraversableAccessor $config) {
-		$fab = $this->_injector->produce($config->getItem('factory'));
+		$qname = $config->getItem('factory');
 
-		if (!($fab instanceof IInjectableFactory)) throw new \ErrorException(sprintf('INJ not a factory "%s"', $config->getItem('factory')));
+		$fab = $this->_driver
+			->getItem('injector')
+			->produce($qname);
 
-		return $fab->produce($config);
+		if (!($fab instanceof IInjectableFactory)) throw new \ErrorException(sprintf('INJ not a factory "%s"', $qname));
+
+		return $fab->produce($this->_getInstanceConfig($config));
 	}
 }
