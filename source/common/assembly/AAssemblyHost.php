c9980b4b0b717b9fc6326358a3dc7d8b74f24842
chkt <ck@chkt.one>
2018-05-18 18:44:52 +0800

    Remove ->_handleAccessorException() Add ->_handleAccessFailure(), ->hasAssembled()

--- source/common/assembly/AAssemblyHost.php
+++ source/common/assembly/AAssemblyHost.php
@@ -3,8 +3,6 @@
 namespace eve\common\assembly;
 
 use eve\common\access\ItemAccessor;
-use eve\common\access\exception\AccessorException;
-use eve\common\assembly\exception\InvalidKeyException;
 use eve\common\assembly\exception\DependencyLoopException;
 
 
@@ -27,27 +25,18 @@ implements IAssemblyHost
 	abstract protected function _produceItem(string $key);
 
 
-	protected function _handleAccessorException(AccessorException $ex) : bool {
-		$key = $ex->getKey();
-
-		if (in_array($key, $this->_stack)) throw new DependencyLoopException($key, $ex);
+	protected function _handleAccessFailure(array & $data, string $key) {
+		if (in_array($key, $this->_stack)) throw new DependencyLoopException($key);
 
 		array_push($this->_stack, $key);
 
-		$this->_useData()[$key] = $this->_produceItem($key);
+		$data[$key] = $this->_produceItem($key);
 
 		array_pop($this->_stack);
-
-		return true;
 	}
 
 
-	public function hasKey(string $key) : bool {
-		try {
-			return parent::hasKey($key) || !is_null($this->getItem($key));
-		}
-		catch (InvalidKeyException $ex){
-			return false;
-		}
+	public function hasAssembled(string $key) : bool {
+		return parent::hasKey($key);
 	}
 }
